03e6612cc547c85617c808dcd3165a0256c8848b
jason <pnxl@pm.me>
2022-09-12 22:47:07 +0700

    what i do section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,26 +1,41 @@
 <template>
   <Navbar />
-  <div class="flex flex-col gap-4">
-    <div class="flex flex-col gap-2">
-      <h2 class="text-3xl font-semibold">{{ randomiseGreeting() }} 👋🏼</h2>
-      <h1
-        class="w-fit animate-gradient-x bg-gradient-to-r from-sky-500 via-indigo-500 to-purple-500 bg-clip-text text-5xl font-bold text-transparent"
-      >
-        {{ randomiseIntro() }}
-      </h1>
-    </div>
-    <p class="text-grey-700 font-medium dark:text-gray-300">
-      Welcome to my small cozy corner of the internet! I'm a 13-year-old guy
-      based in Indonesia, and is a self-taught developer. I'm currently pursuing
-      full-stack development, aiming to create great user interfaces on the
-      front-end, with scalable and secure systems for the back-end. Make
-      yourself comfortable, and I hope you enjoy your stay!
-    </p>
-  </div>
+  <main class="flex flex-col gap-12">
+    <section id="introduction" class="flex flex-col gap-4">
+      <div class="flex flex-col gap-2">
+        <h3 class="text-3xl font-semibold">{{ randomiseGreeting() }} 👋🏼</h3>
+        <h1
+          class="w-fit animate-gradient-x bg-gradient-to-r from-sky-500 via-indigo-500 to-purple-500 bg-clip-text text-5xl font-bold text-transparent"
+        >
+          {{ randomiseIntro() }}
+        </h1>
+      </div>
+      <p class="text-grey-700 font-medium dark:text-gray-300">
+        Welcome to my small cozy corner of the internet! I'm a 13-year-old guy
+        based in Indonesia, and is a self-taught developer. Currently, pursuing
+        full-stack development. Make yourself comfortable, and I hope you enjoy
+        your stay!
+      </p>
+    </section>
+    <section id="what-i-do" class="flex flex-col gap-4">
+      <div class="flex gap-2">
+        <IconThoughtBubble class="my-auto text-3xl" />
+        <h2 class="my-auto text-4xl font-semibold">What do I do?</h2>
+      </div>
+      <p class="text-grey-700 dark:text-gray-300">
+        I like everything that involves technology; from designing and
+        programming different kinds software, to understanding how the internet
+        works together, to cybersecurity, systems, and so much more. I strive to
+        learn more about these things every day, and utilize my knowledge to
+        further understand how or why the technology around us works.
+      </p>
+    </section>
+  </main>
 </template>
 
 <script setup lang="ts">
 import Navbar from "@/components/global/Navbar.vue";
+import IconThoughtBubble from "virtual:icons/mdi/thought-bubble";
 
 const randomiseGreeting = () => {
   const greetings: Array<string> = [
