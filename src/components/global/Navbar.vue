8b4983e68471742aa02b0d3d5c6956a8e279b16f
jason <pnxl@pm.me>
2022-09-12 22:19:05 +0700

    add basic navbar

---
+++ src/components/global/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="mb-20 w-full">
+    <div class="flex gap-8 font-mono">
+      <router-link
+        to="/"
+        :class="
+          $route.path === '/'
+            ? `cursor-default font-semibold text-indigo-600 dark:text-indigo-300`
+            : `transition hover:text-indigo-700 hover:underline dark:hover:text-indigo-200`
+        "
+      >
+        /index
+      </router-link>
+      <router-link
+        to="/about"
+        :class="
+          $route.path.startsWith('/about')
+            ? `cursor-default font-semibold text-indigo-600 dark:text-indigo-300`
+            : `transition hover:text-indigo-700 hover:underline dark:hover:text-indigo-200`
+        "
+      >
+        /about
+      </router-link>
+    </div>
+  </div>
+</template>
