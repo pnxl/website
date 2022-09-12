196d1cc1a7da18bf20c2d3db65e377ca1106d9ce
jason <pnxl@pm.me>
2022-09-12 22:18:53 +0700

    remove transitions

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <template>
   <div
     id="app"
-    class="min-h-screen min-w-full bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-200"
+    class="min-h-screen min-w-full bg-gradient-to-bl from-gray-100 to-[#cfd7e7] text-gray-800 dark:from-[#070a11] dark:to-gray-900 dark:text-gray-200 md:flex md:justify-center"
   >
-    <router-view v-slot="{ Component }">
-      <transition name="fade" mode="out-in">
-        <component :is="Component" />
-      </transition>
-    </router-view>
+    <div class="px-4 py-8 md:w-[48rem] md:py-16">
+      <router-view />
+    </div>
   </div>
 </template>
