517f5f20d3b43a7d0745c9acba3cb51d793fe683
jason <pnxl@pm.me>
2022-09-12 22:24:11 +0700

    oopsies

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,42 +4,40 @@ module.exports = {
   darkMode: "media",
   theme: {
     extend: {
-      extend: {
-        animation: {
-          "gradient-x": "gradient-x 15s ease infinite",
-          "gradient-y": "gradient-y 15s ease infinite",
-          "gradient-xy": "gradient-xy 15s ease infinite",
+      animation: {
+        "gradient-x": "gradient-x 15s ease infinite",
+        "gradient-y": "gradient-y 15s ease infinite",
+        "gradient-xy": "gradient-xy 15s ease infinite",
+      },
+      keyframes: {
+        "gradient-y": {
+          "0%, 100%": {
+            "background-size": "400% 400%",
+            "background-position": "center top",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "center center",
+          },
         },
-        keyframes: {
-          "gradient-y": {
-            "0%, 100%": {
-              "background-size": "400% 400%",
-              "background-position": "center top",
-            },
-            "50%": {
-              "background-size": "200% 200%",
-              "background-position": "center center",
-            },
+        "gradient-x": {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
           },
-          "gradient-x": {
-            "0%, 100%": {
-              "background-size": "200% 200%",
-              "background-position": "left center",
-            },
-            "50%": {
-              "background-size": "200% 200%",
-              "background-position": "right center",
-            },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
           },
-          "gradient-xy": {
-            "0%, 100%": {
-              "background-size": "400% 400%",
-              "background-position": "left center",
-            },
-            "50%": {
-              "background-size": "200% 200%",
-              "background-position": "right center",
-            },
+        },
+        "gradient-xy": {
+          "0%, 100%": {
+            "background-size": "400% 400%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
           },
         },
       },
