14e4015ac590cb95e9b8b2eb17e57a8aaf347a79
jason <pnxl@pm.me>
2022-09-13 19:23:57 +0700

    improve responsiveness

--- src/components/global/Footer.vue
+++ src/components/global/Footer.vue
@@ -1,33 +1,35 @@
 <template>
   <div class="mt-16 flex w-full flex-col gap-8">
     <hr class="border-t-2 border-dashed border-gray-600" />
-    <div class="flex justify-between">
+    <div
+      class="flex flex-col justify-center gap-1 md:flex-row md:justify-between md:gap-0"
+    >
       <div class="flex flex-col gap-1">
-        <div class="flex gap-1 font-semibold">
+        <div class="flex justify-center gap-1 font-semibold">
           <p class="my-auto">Made</p>
           <p class="my-auto">with</p>
           <IconHeart class="my-auto text-indigo-600 dark:text-indigo-400" />
           <p class="my-auto">by</p>
           <p class="my-auto">Jason.</p>
         </div>
-        <span class="text-sm text-gray-600 dark:text-gray-400"
+        <span class="text-center text-sm text-gray-600 dark:text-gray-400"
           >Copyright © 2022 Pix3l_</span
         >
       </div>
-      <div class="flex flex-col gap-1 text-right">
+      <div class="flex flex-col gap-1 text-center md:text-right">
         <a
           href="https://github.com/pnxl/website"
-          class="flex w-full justify-end gap-1 transition hover:text-indigo-700 dark:hover:text-indigo-200"
+          class="hidden w-full justify-end gap-1 transition hover:text-indigo-700 dark:hover:text-indigo-200 md:flex"
         >
           <p class="underline">This project is open source!</p>
           <IconOpenInNew class="my-auto text-sm" />
         </a>
         <router-link
           to="licenses"
-          class="flex w-full justify-end gap-1 text-sm text-gray-600 transition hover:text-indigo-600 dark:text-gray-400 dark:hover:text-indigo-300"
+          class="flex w-full justify-center gap-1 text-sm text-gray-600 transition hover:text-indigo-600 dark:text-gray-400 dark:hover:text-indigo-300 md:justify-end"
         >
           <p class="underline">Licenses and credits</p>
-          <IconArrowRight class="my-auto" />
+          <IconArrowRight class="my-auto hidden md:block" />
         </router-link>
       </div>
     </div>
