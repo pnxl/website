c257ce2baad50382e904974de23586c2d6dd7204
jason <pnxl@pm.me>
2022-09-13 14:09:37 +0700

    lighter backdrop on light mode

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     id="app"
-    class="min-h-screen min-w-full bg-gradient-to-bl from-gray-100 to-[#cfd7e7] text-gray-800 dark:from-[#070a11] dark:to-gray-900 dark:text-gray-200 md:flex md:justify-center"
+    class="min-h-screen min-w-full bg-gradient-to-bl from-gray-100 to-[#dae2f1] text-gray-800 dark:from-[#070a11] dark:to-gray-900 dark:text-gray-200 md:flex md:justify-center"
   >
     <div class="px-4 py-8 md:w-[48rem] md:py-16">
       <router-view />
